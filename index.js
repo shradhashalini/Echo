0e48c8051e71e8b9e12c26338ffac7899afbf31b
Shradha Shalini <shradhashalini09@gmail.com>
2018-11-27 23:48:37 -0600

    Update index.js

--- index.js
+++ index.js
@@ -53,4 +53,6 @@ const bodyParser = require('body-parser');
 const server = express(); 
 server.use(bodyParser.json()); 
 server.post('/hook', app);
-server.listen(3000, () => console.log('Server listening on port 3000.'))
+server.listen(3000, () => {
+              console.log('Server listening on port 3000.');
+});
