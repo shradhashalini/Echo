f6cc17c40c02b5da82b1377d11311e277cb2fedd
Shradha Shalini <shradhashalini09@gmail.com>
2018-11-27 23:25:25 -0600

    Update and rename index_v2.js to index.js

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
 'use strict';
 
+const {
+  dialogflow,
+  Suggestions
+} = require('actions-on-google');
+const functions = require('firebase-functions');
+
+const app = dialogflow({debug: true});
+
+app.intent('Default Fallback Intent', (conv) => {
+    conv.ask('Please repeat');
+});
+
+app.intent('Bathroom', (conv) => {
+    conv.ask('Hi, what do you wanna talk about?');
+    conv.ask(new Suggestions(['fashion tips', 'celebrity news']));
+});
+
+/*
+'use strict';
+
 // Import the Dialogflow module from the Actions on Google client library.
 const {dialogflow} = require('actions-on-google');
 
@@ -16,6 +36,7 @@ app.intent('Bathroom', (conv, {record}) => {
     // Respond with the user's lucky number and end the conversation.
     conv.close('Your bathroom visit is fine.');
 });
+*/
 
 const express = require('express'); 
 const bodyParser = require('body-parser'); 
