feb7e7330b5814b6cd2315e982955ca45b44df0b
Shradha Shalini <shradhashalini09@gmail.com>
2018-11-27 21:51:39 -0600

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const app = dialogflow({debug: true});
 // Handle the Dialogflow intent named 'favorite color'.
 // The intent collects a parameter named 'color'.
 app.intent('Bathroom', (conv, {record}) => {
-    const bathroom_result = "normal";
+    const bathroom_result = 0;
     // Respond with the user's lucky number and end the conversation.
     conv.close('Your bathroom visit is ' + bathroom_result);
 });
